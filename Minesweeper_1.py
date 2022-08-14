b8d062ecb9fb108d2c098dfb094c40a8e4935815
ShreySuri <shreysuri5@gmail.com>
2022-08-13 22:24:03 -0500

    Update Minesweeper_1.py

--- Minesweeper_1.py
+++ Minesweeper_1.py
@@ -1,3 +1,5 @@
+import random
+
 def octal_list(x):
     x = list(oct(x))
     x.pop(0)
@@ -7,4 +9,13 @@ def octal_list(x):
         x[i] = int(x[i])
     return(x)
 
+def example_cords():
+    for i in range (0, 10):
+        decimal = random.randint(0, 64)
+        octal = octal_list(decimal)
+        x = octal[0] + 1
+        y = octal[1] + 1
+        print("%s,%s" % (x, y))
+    return(None)
 
+        
